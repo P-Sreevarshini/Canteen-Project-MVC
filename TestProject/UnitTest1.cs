31914788fc9691ae1dc632b5b8eb1bb9f0be1bb1
neojarvis <techadmin@iamneo.ai>
2024-04-02 10:22:16 +0000

    Auto-save-commit-02/04/2024--15:52:12

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -10,10 +10,12 @@ namespace TestProject
 {
     public class Tests
     {
+         private ApplicationDbContext _context;
         [SetUp]
         public void Setup()
         {
         }
+        
         // Test to check that ApplicationDbContext Contains DbSet for model CanteenOrder
         [Test]
         public void ApplicationDbContext_ContainsDbSet_CanteenOrder()
@@ -123,7 +125,21 @@ namespace TestProject
             Type expectedType = propertyInfo.PropertyType;
             Assert.AreEqual(typeof(int), expectedType, "Property Quantity in CanteenOrder class is not of type int");
         }
-        
+
+        // Test to Check CanteenOrder Models Property OrderDate Exists with correcct datatype DateTime    
+        [Test]
+        public void CanteenOrder_OrderDate_PropertyExists_ReturnExpectedDataTypes_DateTime()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Models.CanteenOrder";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type canteenOrderType = assembly.GetType(typeName);
+            PropertyInfo propertyInfo = canteenOrderType.GetProperty("OrderDate");
+            Assert.IsNotNull(propertyInfo, "Property OrderDate does not exist in CanteenOrder class");
+            Type expectedType = propertyInfo.PropertyType;
+            Assert.AreEqual(typeof(DateTime), expectedType, "Property OrderDate in CanteenOrder class is not of type DateTime");
+        }
+
         // Test to check whether OrderController Controllers Class exists
         [Test]
         public void OrderController_Controllers_ClassExists()
@@ -159,6 +175,64 @@ namespace TestProject
             Assert.IsNotNull(methodInfo, "Method Create does not exist in OrderController class");
         }
 
+        // Test to Check OrderController Controllers Method Create with parameter Exists
+        [Test]
+        public void OrderController_Create_Method_with_ElectionCandidate_Param_Exists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Controllers.OrderController";
+            Assembly assembly = Assembly.Load(assemblyName);
+            string typeName1 = "dotnetapp.Models.CanteenOrder";
+            Type canteenOrderType = assembly.GetType(typeName1);
+            object instance1 = Activator.CreateInstance(canteenOrderType);
+            Type OrderControllerType = assembly.GetType(typeName);
+            object instance = Activator.CreateInstance(OrderControllerType, _context);
+            MethodInfo methodInfo = OrderControllerType.GetMethod("Create", new Type[] { canteenOrderType });
+            Assert.IsNotNull(methodInfo, "Result should not be null");
+        }
+        // test to check that Create method in CandidateController adds new candidate to db
+        [Test]
+        public void Create_in_CandidateController_Add_new_Candidate_to_DB()
+        {
+            string assemblyName = "dotnetapp";
+            Assembly assembly = Assembly.Load(assemblyName);
+            string modelType = "dotnetapp.Models.ElectionCandidate";
+            string controllerTypeName = "dotnetapp.Controllers.CandidateController";
+            Type controllerType = assembly.GetType(controllerTypeName);
+            Type controllerType2 = assembly.GetType(modelType);
+            
+                var teamData = new Dictionary<string, object>
+                    {
+                        { "StudentName", "Demo1" },
+                        { "Standard", "X" },
+                        { "PositionAppliedFor", "Vice-President" },
+                        { "ContactEmail", "Demo1@gmail.com" },
+                        { "CampaignPlatform", "Xyz ABC" }
+                    };
+            var commuter = new ElectionCandidate();
+                foreach (var kvp in teamData)
+                {
+                    var propertyInfo = typeof(ElectionCandidate).GetProperty(kvp.Key);
+                    if (propertyInfo != null)
+                    {
+                        propertyInfo.SetValue(commuter, kvp.Value);
+                    }
+                }
+                MethodInfo method = controllerType.GetMethod("Create", new[] { controllerType2 });
 
-    }
+                if (method != null)
+                {
+
+                    var controller = Activator.CreateInstance(controllerType, _context);
+                    var result = method.Invoke(controller, new object[] { commuter });
+                    var ride = _context.ElectionCandidates.ToList().FirstOrDefault(o => (int)o.GetType().GetProperty("StudentId").GetValue(o) == 1);
+                    Assert.IsNotNull(result);
+                    Assert.AreEqual("X", (string)ride.GetType().GetProperty("Standard").GetValue(ride));
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+            }
+        }
 }
